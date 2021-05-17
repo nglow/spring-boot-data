03f34453c60fe9673f2512dd21530dd74f85ee41
ndoom90 <ndoom90@gmail.com>
2021-05-17 19:04:37 +0900

    In-memory DB - H2

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spring Boot Data
+
+## Contents
+1. 인메모리 데이터베이스
+## Reference
+* [스프링 부트 개념과 활용](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81%EB%B6%80%ED%8A%B8)
---
+++ src/main/java/me/nglow/data/h2/H2Runner.java
@@ -0,0 +1,41 @@
+package me.nglow.data.h2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.Statement;
+
+@Component
+public class H2Runner implements ApplicationRunner {
+
+
+    private final DataSource dataSource;
+
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public H2Runner(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        try(Connection connection = dataSource.getConnection()) {
+            System.out.println(connection.getMetaData().getURL());
+            System.out.println(connection.getMetaData().getUserName());
+
+            Statement statement = connection.createStatement();
+            String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (id));";
+            statement.executeUpdate(sql);
+        }
+
+        // 위 방법보다 JDBC 템플릿을 사용하는것을 추천 -> Connection관리등 여러가지를 처리해주는듯...
+        jdbcTemplate.execute("INSERT INTO USER VALUES (1, 'taekhyeon')");
+    }
+}
